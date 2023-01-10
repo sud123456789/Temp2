7b1f45d1829bf36dd65f64c45ef6cbd1432505be
Sudharani <Sudharani@LAPTOP-HSJQEM5R>
2023-01-10 13:18:13 +0530

    my second commit

---
+++ jenkins2/src/main/java/jenkins2/Demo1.java
@@ -0,0 +1,13 @@
+package jenkins2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test
+	public void m1() {
+		Reporter.log("hii",true);
+	
+	}
+
+}
---
+++ jenkins2/src/main/java/jenkins2/Demo2.java
@@ -0,0 +1,12 @@
+package jenkins2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	@Test
+	public void m2() {
+		Reporter.log("gnt",true);
+	}
+
+}
